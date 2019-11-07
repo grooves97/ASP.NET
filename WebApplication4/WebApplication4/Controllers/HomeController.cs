cd67898fd4f4d740d3a1f4e71e6f6ffa45883896
Аслан Нурсеитов <grooves97@gmail.com>
2019-11-07 17:22:30 +0600

    Lesson 4

---
+++ WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication4.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [OutputCache(Duration = 5)]
+        public ActionResult Time()
+        {
+            var time = DateTime.Now.ToLongTimeString();
+            return Content(time.ToString());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string email, string text)
+        {
+            return Json(new { email,text });
+        }
+    }
+}
