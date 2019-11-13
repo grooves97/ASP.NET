04238284bf35a7ed5d10efe2989d52104c121d84
Аслан Нурсеитов <grooves97@gmail.com>
2019-11-13 18:16:11 +0600

    Web Api lesson /scaffolding

---
+++ WebApplication8/WebApplication8WebApi/WebApplication8WebApi/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication8WebApi.Models
+{
+    public class Book
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; } = DateTime.Now.Year;
+    }
+}
