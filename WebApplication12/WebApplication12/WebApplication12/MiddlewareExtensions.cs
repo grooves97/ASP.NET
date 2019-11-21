f83087b3ecf563f0eadc89065c43f27e8666b256
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-11-21 17:29:47 +0600

    done

---
+++ WebApplication12/WebApplication12/WebApplication12/MiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSafariProcessor(this IApplicationBuilder app)
+        {
+            return app.UseMiddleware<SafariMiddleware>();
+        }
+    }
+}
---
+++ WebApplication12/WebApplication12/WebApplication12/SafariMiddleware.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12
+{
+    public class SafariMiddleware
+    {
+        private readonly RequestDelegate requestDelegate;
+
+        public SafariMiddleware(RequestDelegate requestDelegate)
+        {
+            this.requestDelegate = requestDelegate;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var browser = context.Request.Headers["User-Agent"].ToString();
+            if (browser.ToLower().Contains("safari"))
+            {
+                await context.Response.WriteAsync("Пошёл вон");
+            }
+            await requestDelegate(context);
+        }
+    }
+}
