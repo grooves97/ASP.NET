bf135aa566a80c06aeb3d90979c36b892168c0a5
grooves97 <45422778+grooves97@users.noreply.github.com>
2019-12-10 18:18:55 +0600

    Identity NET.Core

---
+++ WebApplication21/WebApplication21/WebApplication21/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebApplication21.Models;
+
+namespace WebApplication21.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
